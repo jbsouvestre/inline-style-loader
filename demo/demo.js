d25e08b648ec9a5286c4e806b8293483352a1c33
jb <jb@remotium.com>
2015-03-02 18:41:42 -0800

    missing files

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,16 @@
 /*jshint strict: false*/
+
+var inlineStyles = require('./inline.css');
+
 var element = document.createElement('div');
-element.setAttribute('style', require('./inline.css'));
+element.setAttribute('style', inlineStyles['.select']);
 element.innerHTML = 'This is a test D:';
 document.body.appendChild(element);
+
+
+
+
+var button = document.createElement('button');
+button.setAttribute('style', inlineStyles['button']);
+button.innerHTML = 'Click me !';
+document.body.appendChild(button);
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 /*jshint node: true, strict: false*/
-var extractrules = /(\{)[^\}]+(\})/,
-    stripSpecials = /\r?\n|\r/g,
-    brackets = /[\{\}]/g;
+var css = require('css');
+var _ = require('lodash');
+var util = require('util');
+
+var RULE = 'rule';
 
 module.exports = function(source) {
     if (this.cacheable) {
         this.cacheable();
     }
-    var str = source.match(extractrules)[0]
-        .replace(stripSpecials, '')
-        .replace(brackets, '');
-    return str;
+
+    var stylesheet = css.parse(source).stylesheet;
+
+    if (stylesheet.parsingErrors.length) {
+        throw new Error('Parsing Error occured.');
+    }
+
+    var rules = {},
+        inline;
+
+    _.each(stylesheet.rules, function(rule) {
+        inline = '';
+
+        if (rule.type === RULE) {
+            inline = _.map(rule.declarations, function(declaration) {
+                return util.format('%s: %s', declaration.property, declaration.value);
+            }).join('; ');
+        }
+
+        _.each(rule.selectors, function(selector) {
+            rules[selector] = inline;
+        });
+    });
+
+    return rules;
 };
